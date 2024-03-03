69557bf9b8b0d7011568e05d6acb606d10cda61a
Reuben Rob <98816708+SirReuben@users.noreply.github.com>
2024-03-03 13:15:34 +0800

    main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,6 +42,37 @@
   }
 
   /**
+   * About Me
+   */
+  const mask = document.querySelector("#div-mask-back");
+const tl = gsap.timeline();
+
+tl.to(mask, {
+  "--m1": "20%",
+  delay: 2,
+  duration: 0.5,
+  ease: "back.out(2)"
+}).to(mask, {
+  "--m2": "30%",
+  duration: 0.5,
+  delay: 0.5,
+  ease: "back.out(2)"
+});
+
+window.addEventListener("mousemove", (e) => {
+  const { clientX, clientY } = e;
+  const x = Math.round((clientX / window.innerWidth) * 100);
+  const y = Math.round((clientY / window.innerHeight) * 100);
+
+  gsap.to(mask, {
+    "--x": `${x}%`,
+    "--y": `${y}%`,
+    duration: 0.3,
+    ease: "sine.out"
+  });
+});
+
+  /**
    * Navbar links active state on scroll
    */
   let navbarlinks = select('#navbar .scrollto', true)
